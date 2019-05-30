0a744c42e1ae6b782cdc2bac4661d9b3ae9006d3
Raurur00 <javierzavalatorres@gmail.com>
2019-05-30 18:58:52 -0400

    upload file

--- server/bioinf/routes/index.js
+++ server/bioinf/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+var formidable = require('formidable');
 
 /* GET home page. */
 router.get('/prueba', function(req, res, next) {
@@ -35,6 +36,24 @@ router.get('/prueba', function(req, res, next) {
     }
     console.log(genomas);
     res.json({genomas});
+  });
+});
+
+router.post('/fileupload', function(req, res, next) {
+  var form = new formidable.IncomingForm();
+  form.uploadDir = "./data"
+  form.parse(req, function (err, fields, files){
+    if(err) throw err;
+    res.end();
+    /*
+    var oldpath = files.filetoupload.path;
+    var newpath = 'C:/Users/Your Name/' + files.filetoupload.name;
+    fs.rename(oldpath, newpath, function (err) {
+      if (err) throw err;
+      res.write('File uploaded and moved!');
+      res.end();
+    });
+    */
   });
 });
 
