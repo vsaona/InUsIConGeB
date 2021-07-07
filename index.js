03f9790fb866ec371ecaa790ef783cc3f3e36768
Vicente Saona <vicente.saona@sansano.usm.cl>
2021-07-06 23:49:10 -0400

    adapted the 'update database call'

--- index.js
+++ index.js
@@ -529,6 +529,7 @@ app.post('/updateDatabases', function(req, res, next) {
     // Last, we update BLAST databases
     if("password" == req.body.blast) {
       for(var i = 0; i < 26; i++) {
+        var twoDigitIndex = i < 10 ? "0" + i : i
         shelljs.exec(`wget https://ftp.ncbi.nlm.nih.gov/blast/db/refseq_protein.${i}.tar.gz -O ../blast/refseq_protein/refseq_protein.${i}.tar.gz`);
         shelljs.exec(`gzip --decompress --force ../blast/refseq_protein/refseq_protein.${i}.tar.gz`);
       }
